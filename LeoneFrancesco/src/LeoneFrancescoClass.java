80adc8ea4637f91e94c4365bc4ec470fe91afb1b
LeoneFrancesco <Leone@pianodue16>
2023-02-04 10:51:46 +0100

    Versione 1: corretta

--- LeoneFrancesco/src/LeoneFrancescoClass.java
+++ LeoneFrancesco/src/LeoneFrancescoClass.java
@@ -22,9 +22,15 @@ public class LeoneFrancescoClass {
 	 */
 	
 	public int leoneMet(int s,int f) {
-		int r=0;
-		for(int i=1;i<f;i++) {
-			r=r+r;
+		//Errore 1: la variabile contatore del ciclo "i" deve essere inizializzata con il valore della variabile "s"
+		//Errore 2: il ciclo deve continuare ad essere eseguito fino al valore di "f" (incluso) 
+		//Errore 3: La variabile r deve contenere la produttoria. Quindi deve effetuare la moltiplicazione
+		//			tra se stessa e il valore della variabile "i"
+		//Errore 4: la variabile "r" deve essere inizializzata con il valore 1, in modo che non influisca con il calcolo
+		//			della moltiplicazione
+		int r=1;
+		for(int i=s;i<=f;i++) {
+			r=r*i;
 		}
 		return r;
 	}
